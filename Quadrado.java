25cb88137cd09b7fde6b56f7b79b0714878867a0
Walney Negreiros Gomes <36850527+walneygomes@users.noreply.github.com>
2019-04-18 14:07:11 -0300

    Add files via upload

---
+++ Quadrado.java
@@ -0,0 +1,62 @@
+package triangulo;
+
+public class Quadrado {
+
+	private static Quadrado intance = null;
+	private static final int MULT = 2;
+	private double width;
+	private double height;
+
+	private Quadrado(double width, double height) {
+		super();
+		this.width = width;
+		this.height = height;
+	}
+
+	public static Quadrado instance(double width, double height) {
+		if (intance == null) {
+			intance = new Quadrado(width, height);
+
+		}
+		return intance;
+
+	}
+
+	public double areaQuadrado() {
+		return (this.getHeight() * this.getWidth());
+	}
+
+	public double perimetro() {
+
+		return (this.getHeight() + this.getWidth()) * MULT;
+	}
+
+	public double diagonal() {
+		return Math.sqrt(this.getHeight() * this.getWidth() + this.getHeight() * this.getWidth());
+	}
+
+	public String toString() {
+		return "Perimetro" + this.perimetro() + "Diagonal" + this.diagonal() + "AreaTotal" + this.areaQuadrado();
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public static int getMult() {
+		return MULT;
+	}
+
+}
